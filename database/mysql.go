ea2426ae62121835072e725de2ed784a7c9f63d6
Thomas Ruiz <contact@thomasruiz.eu>
2020-01-18 20:12:58 +0100

    chore(project): tidy go.mod and moved to sqlx

--- database/mysql.go
+++ database/mysql.go
@@ -1,13 +1,13 @@
 package database
 
 import (
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 	"github.com/scribletop/scribletop-api/config"
 )
 
-func Connect(config config.DatabaseConfig) (*gorm.DB, error) {
-	return gorm.Open(
+func Connect(config config.DatabaseConfig) (*sqlx.DB, error) {
+	return sqlx.Connect(
 		"mysql",
 		config.Username+":"+config.Password+"@("+config.Hostname+")/"+config.Database+"?charset=utf8&parseTime=True&loc=Local",
 	)
