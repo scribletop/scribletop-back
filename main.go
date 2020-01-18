7a8502400661b00df425d03112fdf44e85b1f1c8
Thomas Ruiz <contact@thomasruiz.eu>
2020-01-18 14:32:13 +0100

    chore(project): init project

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"os"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	if err := r.Run(); err != nil {
+		os.Exit(1)
+	}
+}
