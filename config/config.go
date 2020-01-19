0c4decf6fb4ad402d1f89035acaedb1f5d6cb862
Thomas Ruiz <contact@thomasruiz.eu>
2020-01-19 21:17:59 +0100

    feat(logs): improved logs

--- config/config.go
+++ config/config.go
@@ -1,6 +1,10 @@
 package config
 
-import "os"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
 type DatabaseConfig struct {
 	Username string
@@ -12,16 +16,23 @@ type DatabaseConfig struct {
 type Config struct {
 	Database DatabaseConfig
 	Env      string
+	Debug    bool
 }
 
 func Load() Config {
 	config := Config{
 		DatabaseConfig{"postgres", "password", "localhost", "scribletop"},
 		"local",
+		false,
 	}
 
 	if val := os.Getenv("ENV"); val != "" {
 		config.Env = val
+	}
+
+	if val := os.Getenv("DEBUG"); val != "" {
+		debug, err := strconv.ParseBool(val)
+		config.Debug = err != nil || debug
 	}
 
 	if val := os.Getenv("DB_USERNAME"); val != "" {
@@ -41,4 +52,17 @@ func Load() Config {
 	}
 
 	return config
+}
+
+func Print(c Config) {
+	fmt.Printf("------------------------------------------------\n")
+	fmt.Printf("Running scribletop API")
+	fmt.Printf("Configuration loaded:\n\n")
+	fmt.Printf("ENV:        %s\n", c.Env)
+	fmt.Printf("DEBUG MODE: %t\n\n", c.Debug)
+	fmt.Printf("Database:\n")
+	fmt.Printf("	Hostname: %s\n", c.Database.Hostname)
+	fmt.Printf("	Username: %s\n", c.Database.Username)
+	fmt.Printf("	Database: %s\n", c.Database.Database)
+	fmt.Printf("------------------------------------------------\n")
 }
